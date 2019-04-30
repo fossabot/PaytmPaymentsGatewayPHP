a825852035397bf2221af8084b1ff37127472dcf
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-04-30 18:53:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,8 @@ echo $order;
                     <input class="input100" type="hidden" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="<?php
 echo $cust;
 ?>" placeholder="Mobile Number" required>
-                    <input class="input100" type="hidden" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="RETAIL" placeholder="RETAIL">
+                    <input class="input100" type="hidden" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="RETAIL" >
+                    <input class="input100" type="hidden" tabindex="4" maxlength="12" size="12" name="CHANNEL_ID" autocomplete="off" value="WAP" >
                     <input class="input100" tabindex="10" type="number" name="TXN_AMOUNT" value=""" placeholder="Amount" required>
                 </div>
 
